a7a4d8e63c6cc40b9db315415e854fe239ac39fd
Blooming Burden <flowersoverskulls96@gmail.com>
2023-07-04 22:14:42 +0300

    Change

--- src/js/modules/myMask.js
+++ src/js/modules/myMask.js
@@ -4,6 +4,9 @@ let firstMask = null;
 
 const setMaskForm = (code) => {
     let element = document.querySelector('.form-sign__phone input');
+
+    if (!element) return;
+    
     let maskOptions = {
         mask: `+{${code}} (000)000-00-00`,
     };
